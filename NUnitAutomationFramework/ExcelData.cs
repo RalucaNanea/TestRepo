50329c81654ce10ef3ebde8fe7a6e1d7ec7171af
Raluca Nanea <raluca.nanea@veinteractive.com>
2015-12-04 15:09:50 +0800

    Add Packages

---
+++ NUnitAutomationFramework/ExcelData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitAutomationFramework
+{
+    public class ExcelData
+    {
+        public string clientUrl { get; set; }
+        public string testStatus { get; set; }
+    }
+}
---
+++ NUnitAutomationFramework/Selectors.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitAutomationFramework
+{
+
+    public class Selectors : PropertyAttribute
+    {
+        public string websiteUrl { get; set; }
+        public string closeAdvertise { get; set; }
+        public string category { get; set; }
+        public string firstProduct { get; set; }
+        public string[] elementsToSelect { get; set; }
+        public string btnAddToCart { get; set; }
+        public string goToCart { get; set; }
+
+    }
+}
