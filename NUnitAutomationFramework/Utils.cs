3746f47ae15a7be44a74602bd161523f06b7876d
Raluca Nanea <raluca.nanea@veinteractive.com>
2015-12-08 12:04:02 +0800

    this is a commit coming from lastBranch

--- NUnitAutomationFramework/Utils.cs
+++ NUnitAutomationFramework/Utils.cs
@@ -20,7 +20,7 @@ using DocumentFormat.OpenXml;
 namespace NUnitAutomationFramework
 {
     public static class Utils
-    { //this is a test from branch test
+    { //this is lastBranch
         // I am chaging master and I want this change
         public static bool IsElementDisplayed(this IWebDriver driver, By element)
         {
