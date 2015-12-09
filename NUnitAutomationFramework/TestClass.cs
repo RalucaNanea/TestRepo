86208c78faa442c1381500ff0fcdde084a752b13
Raluca Nanea <raluca.nanea@veinteractive.com>
2015-12-09 15:25:05 +0800

    Added test class and changed Utils

---
+++ NUnitAutomationFramework/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitAutomationFramework
+{ //3:24
+    class TestClass
+    {
+    }
+}
